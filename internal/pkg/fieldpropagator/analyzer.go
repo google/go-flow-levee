ff9436b31e9a98c1400adaf48c1f020d6bb31cca
Michaël Lévesque-Dion <mlevesquedion@google.com>
2020-08-05 16:13:34 -0400

    add minimal doc comment

--- internal/pkg/fieldpropagator/analyzer.go
+++ internal/pkg/fieldpropagator/analyzer.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package fieldpropagator implements identification of field propagators.
+// A field propagator is a function that returns a source field.
 package fieldpropagator
 
 import (
