38234e2b06eb020c49921167a93407bf4147b7e2
Patrick Rhomberg <patrhom@google.com>
2020-08-17 23:24:00 +0000

    Remove dead configuration logic.
    Analysis scope and allow-listing is not currently supported.  Associate fields are not used.
    Propagation is now tracked directly via the SSA Value-Instruction graph.  Explicit config is no longer needed.

--- internal/pkg/levee/levee.go
+++ internal/pkg/levee/levee.go
@@ -63,21 +63,6 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				case fieldPropagators.Contains(v):
 					sources = append(sources, source.New(v, conf))
 
-				case conf.IsPropagator(v):
-					// Handling the case where sources are propagated to io.Writer
-					// (ex. proto.MarshalText(&buf, c)
-					// In such cases, "buf" becomes a source, and not the return value of the propagator.
-					// TODO Do not hard-code logging sinks usecase
-					// TODO  Handle case of os.Stdout and os.Stderr.
-					// TODO  Do not hard-code the position of the argument, instead declaratively
-					//  specify the position of the propagated source.
-					// TODO  Consider turning propagators that take io.Writer into sinks.
-					if a := getArgumentPropagator(conf, v); a != nil {
-						sources = append(sources, source.New(a, conf))
-					} else {
-						sources = append(sources, source.New(v, conf))
-					}
-
 				case conf.IsSink(v):
 					c := makeCall(v)
 					for _, s := range sources {
@@ -106,19 +91,4 @@ func report(pass *analysis.Pass, source *source.Source, sink ssa.Node) {
 	b.WriteString("a source has reached a sink")
 	fmt.Fprintf(&b, ", source: %v", pass.Fset.Position(source.Node().Pos()))
 	pass.Reportf(sink.Pos(), b.String())
-}
-
-func getArgumentPropagator(c *config.Config, call *ssa.Call) ssa.Node {
-	if call.Call.Signature().Params().Len() == 0 {
-		return nil
-	}
-
-	firstArg := call.Call.Signature().Params().At(0)
-	if c.PropagatorArgs.ArgumentTypeRE.MatchString(firstArg.Type().String()) {
-		if a, ok := call.Call.Args[0].(*ssa.MakeInterface); ok {
-			return a.X.(ssa.Node)
-		}
-	}
-
-	return nil
 }
