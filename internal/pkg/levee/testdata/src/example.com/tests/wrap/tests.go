a7df89c1a9daeca26d0c9425b7cdcda39bfa2f31
Michaël Lévesque-Dion <mlevesquedion@google.com>
2020-08-26 11:05:31 -0400

    add failing tests

---
+++ internal/pkg/levee/testdata/src/example.com/tests/wrap/tests.go
@@ -0,0 +1,49 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wrap
+
+import (
+	"example.com/core"
+)
+
+func CreateSource() core.Source {
+	return core.Source{}
+}
+
+func TestWrappedCall() {
+	core.Sink(CreateSource()) // want "a source has reached a sink"
+}
+
+func TestNonWrappedCall() {
+	s := CreateSource()
+	core.Sink(s) // want "a source has reached a sink"
+}
+
+func TestWrappedRecv(sources <-chan core.Source) {
+	core.Sink(<-sources) // want "a source has reached a sink"
+}
+
+func TestNonWrappedRecv(sources <-chan core.Source) {
+	s := <-sources
+	core.Sink(s) // want "a source has reached a sink"
+}
+
+func TestWrappedIndex(sources [1]core.Source) {
+	core.Sink(sources[0]) // want "a source has reached a sink"
+}
+func TestNonWrappedIndex(sources [1]core.Source) {
+	s := sources[0]
+	core.Sink(s) // want "a source has reached a sink"
+}
