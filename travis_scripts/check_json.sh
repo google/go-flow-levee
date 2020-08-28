4ea18e586cbebdc7efcaafc8c51bf148aef8fcfb
Michaël Lévesque-Dion <mlevesquedion@google.com>
2020-08-28 11:02:32 -0400

    use custom script

---
+++ travis_scripts/check_json.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+echo "Checking JSON files..."
+failed=false
+while read -r -d '' f; do 
+	if ! OUT=$(jq type "$f"); then
+		echo "$OUT"
+		failed=true
+	fi
+done < <(find . -iname '*.json' -print0)
+if $failed; then
+	exit 1
+fi
+
