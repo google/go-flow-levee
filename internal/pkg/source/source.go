5b35c965bf14461a9f4710e8d64dde0177803643
Michaël Lévesque-Dion <mlevesquedion@google.com>
2020-09-03 15:52:58 -0400

    rebase + fix conflict

--- internal/pkg/source/source.go
+++ internal/pkg/source/source.go
@@ -77,9 +77,7 @@ func (a *Source) dfs(n ssa.Node) {
 		a.record(instr)
 	}
 
-	if n.Referrers() != nil {
-		a.visitReferrers(n.Referrers())
-	}
+	a.visitReferrers(n)
 
 	var operands []*ssa.Value
 	operands = n.Operands(operands)
@@ -100,8 +98,35 @@ func (a *Source) record(target ssa.Instruction) {
 	}
 }
 
-func (a *Source) visitReferrers(referrers *[]ssa.Instruction) {
-	for _, r := range *referrers {
+func (a *Source) visitReferrers(n ssa.Node) {
+	referrers := a.referrersToVisit(n)
+
+	for _, r := range referrers {
+		if a.marked[r.(ssa.Node)] {
+			continue
+		}
+
+		switch v := r.(type) {
+		case *ssa.Call:
+			if a.config.IsSanitizer(v) {
+				a.sanitizers = append(a.sanitizers, &sanitizer.Sanitizer{Call: v})
+			}
+
+		case *ssa.FieldAddr:
+			if !a.config.IsSourceFieldAddr(v) {
+				continue
+			}
+		}
+
+		a.dfs(r.(ssa.Node))
+	}
+}
+
+func (a *Source) referrersToVisit(n ssa.Node) (referrers []ssa.Instruction) {
+	if n.Referrers() == nil {
+		return
+	}
+	for _, r := range *n.Referrers() {
 		// If the referrer is in a different block from the one we last visited,
 		// and it can't be reached from the block we are visiting, then stop visiting.
 		if rb := r.Block(); a.lastBlockVisited != nil &&
@@ -110,39 +135,26 @@ func (a *Source) visitReferrers(referrers *[]ssa.Instruction) {
 			continue
 		}
 
-		if a.marked[r.(ssa.Node)] {
-			continue
-		}
-		switch v := r.(type) {
-		case *ssa.Call:
+		if c, ok := r.(*ssa.Call); ok {
 			// This is to avoid attaching calls where the source is the receiver, ex:
 			// core.Sinkf("Source id: %v", wrapper.Source.GetID())
-			if recv := v.Call.Signature().Recv(); recv != nil && a.config.IsSource(utils.Dereference(recv.Type())) {
+			if recv := c.Call.Signature().Recv(); recv != nil && a.config.IsSource(utils.Dereference(recv.Type())) {
 				continue
-			}
-
-			if a.config.IsSanitizer(v) {
-				a.sanitizers = append(a.sanitizers, &sanitizer.Sanitizer{Call: v})
 			}
 
 			// If this call's index is lower than the highest in its block,
 			// then this call is "in the past" and we should stop traversing.
 			i, ok := indexInBlock(r)
 			if !ok {
-				break
+				continue
 			}
-
 			if i < a.maxInstrReached[r.Block()] {
 				continue
 			}
-
-		case *ssa.FieldAddr:
-			if !a.config.IsSourceFieldAddr(v) {
-				continue
-			}
 		}
-		a.dfs(r.(ssa.Node))
+		referrers = append(referrers, r)
 	}
+	return referrers
 }
 
 func (a *Source) canReach(start *ssa.BasicBlock, dest *ssa.BasicBlock) bool {
