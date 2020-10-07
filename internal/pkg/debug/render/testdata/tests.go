d080bb523acc75e596460193d8fc95c864456c0f
mlevesquedion <mlevesquedion@google.com>
2020-10-07 14:42:56 -0400

    Debugging: improve SSA rendering and add tests (#133)
    * improve rendering and add tests
    
    * fix CI
    
    * fix CI for real this time (need to use extended grep)

---
+++ internal/pkg/debug/render/testdata/tests.go
@@ -0,0 +1,50 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"image"
+)
+
+func TestSingleBlock() {
+	p := image.Point{1, 2}
+	p.X = 3
+	p.Y = 4
+	fmt.Println(p.X + p.Y)
+}
+
+func TestMultiBlock() {
+	p := image.Point{1, 2}
+	if p.X > 0 {
+		if p.Y > 0 {
+			fmt.Printf("in top right quadrant, at (%d, %d)\n", p.X, p.Y)
+		}
+	} else {
+		fmt.Println("somewhere")
+	}
+}
+
+func TestParams(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func TestClosure() {
+	x := 0
+	f := func(x int) {
+		fmt.Println(x)
+	}
+	f(x)
+}
