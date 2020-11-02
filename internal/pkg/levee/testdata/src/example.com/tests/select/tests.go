46651b66e2ac0bad4e6d8f31d1c8ec88201d0e73
Michaël Lévesque-Dion <mlevesquedion@google.com>
2020-11-02 13:49:02 -0500

    rewrite todo for consistency with other similar todos

--- internal/pkg/levee/testdata/src/example.com/tests/select/tests.go
+++ internal/pkg/levee/testdata/src/example.com/tests/select/tests.go
@@ -70,9 +70,9 @@ func TestTaintedAndSinkedInDifferentBranches(objects chan interface{}) {
 	select {
 	case objects <- core.Source{}:
 	case s := <-objects:
-		// we do not expect a report here, because objects is only tainted if the
+		// TODO: we do not expect a report here, because objects is only tainted if the
 		// other branch is taken, and only one branch can be taken
-		// TODO core.Sink(s)
+		core.Sink(s) // want "a source has reached a sink"
 		_ = s
 	}
 }
