d6133b8917e9fff449a123926abc19584ef9a195
Vinayak Goyal <vinaygo@google.com>
2020-12-19 22:08:26 -0800

    lower case local variables.

--- hack/new-findings.sh
+++ hack/new-findings.sh
@@ -5,13 +5,13 @@ set -ex
 install_and_run_levee_against_k8s_from_branch() {
     local branch_name=$1
     git checkout "$branch_name"
-    local LEVEE_OUTPUT="$(mktemp -d)/levee"
-    go build -o "${LEVEE_OUTPUT}" "${LEVEE_DIR}/cmd/levee"
-    CFG_PATH="$(realpath ${K8S_SRC_PATH}/hack/testdata/levee/levee-config.yaml)"
+    local levee_output="$(mktemp -d)/levee"
+    go build -o "${levee_output}" "${LEVEE_DIR}/cmd/levee"
+    local cfg_path="$(realpath ${K8S_SRC_PATH}/hack/testdata/levee/levee-config.yaml)"
     make -C "${K8S_SRC_PATH}" clean
     FINDINGS=$(mktemp)
     set +e
-    make -C "${K8S_SRC_PATH}" vet WHAT="-vettool=$LEVEE_OUTPUT -config=$CFG_PATH" 2>"${FINDINGS}"
+    make -C "${K8S_SRC_PATH}" vet WHAT="-vettool=$levee_output -config=$cfg_path" 2>"${FINDINGS}"
     set -e
 }
 
@@ -20,7 +20,7 @@ LEVEE_DIR=$(dirname $(dirname $(realpath $0)))
 K8S_SRC_PATH="$(go env GOPATH)/src/k8s.io/kubernetes"
 FEATURE_BRANCH=$(git branch --show-current)
 
-if [[ ${FEATURE_BRANCH} == "master" ]]; then
+if [[ "${FEATURE_BRANCH}" == "master" ]]; then
     echo  "Please run from feature branch"
     exit 1
 fi
