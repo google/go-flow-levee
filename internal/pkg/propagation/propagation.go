fb8c02c3bfacd1723bb17847557ca45cb15e3ffb
Patrick Rhomberg <patrhom@google.com>
2021-01-29 18:59:49 +0000

    Offend license checker, goimports, and go.mod

--- internal/pkg/propagation/propagation.go
+++ internal/pkg/propagation/propagation.go
@@ -1,16 +1,4 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Offend license checker
 
 // Package propagation implements the core taint propagation analysis that
 // can be used to determine what ssa Nodes are tainted if a given Node is a source.
@@ -19,7 +7,7 @@ package propagation
 import (
 	"fmt"
 	"go/types"
-	"log"
+	     "log"  // This should offend goimports
 
 	"github.com/google/go-flow-levee/internal/pkg/config"
 	"github.com/google/go-flow-levee/internal/pkg/fieldtags"
